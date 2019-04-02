0354c8c0cbab78e76618caaff39ea1628331b658
Wachcio <wachcio@poczta.fm>
2019-04-03 00:51:25 +0200

    Install axios and lodash

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,67 @@
 <template>
   <div id="app">
     <div class="container">
-      <Search/>
+      <div class="searchWrapper">
+        <h1>Wpisz szukany tytuł</h1>
+        <form action>
+          <input type="search" name="search" id="search">
+        </form>
+      </div>
       <Result/>
     </div>
   </div>
 </template>
 
 <script>
-import Search from "./components/Search";
 import Result from "./components/Result";
 
 export default {
   name: "app",
   components: {
-    Search,
     Result
+  },
+  data() {
+    return {
+      books
+    };
   }
 };
 </script>
 
 <style lang="scss">
+*,
+::after,
+::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100vw;
+  font-family: "Roboto", sans-serif;
+  font-size: 17px;
+
+  & h1 {
+    font-size: 2em;
+  }
+}
+
+.container {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em;
+
+  & .searchWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & form {
+      margin: 1em;
+    }
+  }
+}
 </style>
