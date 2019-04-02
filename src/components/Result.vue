2c0ebacd7b678f96cd168f68f040f72447b9b761
Wachcio <wachcio@poczta.fm>
2019-04-03 00:22:39 +0200

    Initial commit

---
+++ src/components/Result.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="resultWrapper">rezultat</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  computed: {},
+  created() {},
+  watch: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="searchWrapper">Input</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  computed: {},
+  created() {},
+  watch: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
